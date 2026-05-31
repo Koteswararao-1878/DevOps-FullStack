d5a34529767a6dd9de826c52804bd989e68908ff
Koteswararao-1878 <2303a51878@sru.edu.in>
2026-05-31 12:36:20 +0530

    fix: remove cloudinary import from frontend App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, createContext, useContext } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { v2 as cloudinary } from 'cloudinary'
 
 // Pages
 import Home from "./pages/Home";
